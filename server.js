43f3e15897f74c5c3e215f81a6c2c633d9f5ac89
Samer Balee <samerbalee@gmail.com>
2022-02-27 12:30:26 +1100

    added view by manager and add employee functions

--- server.js
+++ server.js
@@ -11,18 +11,18 @@ db.connect(err => {
     if (err) throw err;
     console.log('connected as id ' + db.threadId);
     displayTitle();
-  });
-    
-  displayTitle = () => {
+});
+
+displayTitle = () => {
     console.log("***********************************")
     console.log("*                                 *")
     console.log("*        EMPLOYEE MANAGER         *")
     console.log("*                                 *")
     console.log("***********************************")
     start();
-  };
+};
 
-  async function start() {
+async function start() {
     const userChoice = await inquirer.prompt(questions.firstQuestion);
     switch (userChoice.choices) {
         case "View All Employees":
@@ -56,7 +56,7 @@ db.connect(err => {
             removeRole();
             break;
         case "View All Departments":
-           viewDepartments();
+            viewDepartments();
             break;
         case "Add Department":
             addDepartment();
@@ -77,12 +77,16 @@ db.connect(err => {
 }
 
 function viewEmployees() {
+
+
     db.query(`SELECT employee.id, employee.first_name, employee.last_name, 
-    roles.title AS title, roles.salary, department.department_name AS department 
-    FROM employee  JOIN roles ON employee.role_id = roles.id 
-    JOIN department ON roles.department_id = department.id`, function (err, res) {
+    roles.title AS title, roles.salary, department.department_name AS department, 
+    CONCAT (manager.first_name, " ", manager.last_name) AS manager
+    FROM employee JOIN roles ON employee.role_id = roles.id 
+     JOIN department ON roles.department_id = department.id
+     LEFT JOIN employee AS manager ON employee.manager_id = manager.id;`, function (err, res) {
         if (err) throw err;
-       
+
         console.table(res);
         start();
     });
@@ -90,25 +94,69 @@ function viewEmployees() {
 
 function viewEmpByDepartment() {
     db.query("SELECT * FROM department", async (err, department) => {
-        
-        const {
-            departmentName
-        } = await inquirer.prompt([{
-            type: "list",
-            message: "Select a Department:",
-            name: "departmentName",
-            choices: () => {
-                return department.map((department) => department.department_name);
-            }
-        }]);
+
+        if (err) throw err;
+
+        const { departmentName } = await inquirer.prompt([
+            {
+                type: "list",
+                message: "Select a Department:",
+                name: "departmentName",
+                choices: () => {
+                    return department.map((department) => department.department_name);
+                }
+            }]);
+
         db.query(`SELECT employee.id, employee.first_name, employee.last_name, roles.title, roles.salary, department.department_name AS department
          FROM employee LEFT JOIN roles ON employee.role_id = roles.id 
         LEFT JOIN department ON roles.department_id = department.id`, function (err, res) {
             if (err) throw err;
-            
+
             console.table(res.filter((name) => departmentName === name.department));
             start();
         });
+    })
+}
+
+function viewEmpByManager() {
+    db.query("SELECT * FROM employee", async (err, employee) => {
+
+        if (err) throw err;
+        // get the name, category, starting bid from user
+        const { managerId } = await inquirer.prompt([
+            {
+                type: "list",
+                message: "Choose a manager:",
+                name: "manager_id",
+                choices: () => {
+                    return employee.map((manager) => manager.manager_id);
+                },
+            },]);
+        db.query(`SELECT first_name, last_name FROM employee WHERE manager_id=${manager_id}`, function (err, res) {
+            if (err) throw err;
+            // Log all results of the SELECT statement
+            console.table(res);
+            start();
+        });
+    })
+
+}
+
+async function addEmployee() {
+    let qry = "SELECT id as value, CONCAT(first_name, ' ', last_name) as name FROM employee"
+    db.query(qry, async (err, employees) => {
+        qry = "SELECT id as value, title as name FROM roles"
+        db.query(qry, async (err, roles) => {
+            // get the name, category, starting bid from user
+            const newEmp = await inquirer.prompt(questions.addEmployee(roles, employees));
+            qry = "INSERT INTO employee SET ?"
+            db.query(qry, newEmp, function (err) {
+                if (err) throw err;
+                console.log("New employee was added successfully!");
+                // re-prompt the user for if they want to bid or post
+                start();
+            });
+        })
     })
 }
 
